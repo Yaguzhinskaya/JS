ce31a8a8f01e8340bfeca144f1830953915ffedb
Настенька <nastenka.kuzmina.1996@mail.ru>
2023-02-02 21:23:28 +0300

    commit HW

--- HW/HW1/script.js
+++ HW/HW1/script.js
@@ -1,4 +1,11 @@
+// Задание 1
 
-let num = 123;;
-console.log(num);
+// let tC = 12;
+// console.log(tF = 9 / 5 * tC + 32);
 
+
+// Задание 2
+
+let name = ('Анастасия');
+let admin = name;
+console.log(admin);

4e07091200af577899ff3dc49807811683436206
Настенька <nastenka.kuzmina.1996@mail.ru>
2023-02-04 20:43:34 +0300

    inital commit

--- Sem/Sem1/script.js
+++ Sem/Sem1/script.js
@@ -1,6 +1,34 @@
 
 // console.log('Текс из индекс script.js');
 
-let num = 123;;
-console.log(num);
+
+
+// 1 Создайте переменную num и присвойте ей значение 123
+// Выведите значение этой переменной в консоль
+// let num = 123;;
+// console.log(num);
+
+// 2 Создайте переменную а и присвойте ей значение 5,
+// переопределите переменную а на значение 7, выведите
+// переменную а в консоль
+// let a = 5;
+// a = 7;
+// console.log(a);
+
+// 3
+// let c = 10;
+// let d = 2;
+// let sum = c + d;
+// let dif = c - d;
+// let melt = c * d;
+// let div = c / d;
+// console.log(sum,dif,melt,div);
+
+// 5.
+// let a = 1.5;
+// let b = 0.75;
+// console.log(a + b);
+
+// let A = -10;
+// console.log(-a);
 
